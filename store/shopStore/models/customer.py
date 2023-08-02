a3cf7ed83aebbb56f820a616ad1e96ac4834b482
khalid586 <khalidcomilla58@gmail.com>
2023-08-03 01:11:12 +0600

    customer model added in models for customer info

---
+++ store/shopStore/models/customer.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    phone = models.CharField(max_length = 15)
+    email = models.EmailField()
+    password = models.CharField(max_length = 500)
