7f93072e746c4c0622eeb5d37ecda95664de82a9
khalid586 <khalidcomilla58@gmail.com>
2023-08-02 15:56:55 +0600

    product name price and category added in admin dashboard by creating a adminProduct class and passing it to product admin site

--- store/shopStore/admin.py
+++ store/shopStore/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models.product import Product
 from .models.category import Category
 
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name','price','category']
+
+
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product,AdminProduct)
 admin.site.register(Category)
