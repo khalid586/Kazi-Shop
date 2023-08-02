2bcc7c550a842466273c240747679770d08f3f7c
khalid586 <khalidcomilla58@gmail.com>
2023-08-03 00:04:39 +0600

    category section added in homepage

--- store/shopStore/models/category.py
+++ store/shopStore/models/category.py
@@ -5,3 +5,7 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name 
+    
+    @staticmethod
+    def get_all_categories():
+        return Category.objects.all()
--- store/shopStore/views.py
+++ store/shopStore/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models.product import Product
+from .models.category import Category
 
 # Create your views here.
 def index(request):
-    prods = Product.objects.all()
-    return render(request , 'orders/order.html') 
-    # return render(request , 'index.html', {'products':prods}) 
+    products = Product.objects.all()
+    categories = Category.objects.all()
+    data = {}
+    data['products'] = products
+    data['categories'] = categories
+    return render(request , 'index.html', data) 
