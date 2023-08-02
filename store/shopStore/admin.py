c6cadbb1575c688b47fcdf63b7998be3d4de75fd
khalid586 <khalidcomilla58@gmail.com>
2023-08-02 16:01:05 +0600

    Category name shown in admin panel by creating class

--- store/shopStore/admin.py
+++ store/shopStore/admin.py
@@ -6,7 +6,9 @@ from .models.category import Category
 class AdminProduct(admin.ModelAdmin):
     list_display = ['name','price','category']
 
+class AdminCategory(admin.ModelAdmin):
+    list_display = ['name']
 
 # Register your models here.
 admin.site.register(Product,AdminProduct)
-admin.site.register(Category)
+admin.site.register(Category,AdminCategory)
