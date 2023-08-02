0cacc4967f44cad54b2544198fddb690c55eb5d5
khalid586 <khalidcomilla58@gmail.com>
2023-08-02 16:30:09 +0600

    templates created and server on homepage

--- store/shopStore/views.py
+++ store/shopStore/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse('hello')
+    return render(request , 'index.html')
