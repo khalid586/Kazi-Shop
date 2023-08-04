a4e2130b7316b2854108096dde44e6c2de7b0357
khalid586 <khalidcomilla58@gmail.com>
2023-08-05 02:44:55 +0600

    Error removed ( / was missing in url path)

--- store/shopStore/urls.py
+++ store/shopStore/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('',Index.as_view() , name = 'homepage'),
     path('signup/',Signup.as_view() , name = 'signup'),
     path('login/',Login.as_view() , name = 'login'),
-    path('logout',logout,name = 'logout'),
-    path('cart',cart,name = 'cart'),
+    path('logout/',logout,name = 'logout'),
+    path('cart/',cart,name = 'cart'),
 ]
